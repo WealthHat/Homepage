30d18526fc3d576be0e4e2183e947b7108bffdec
Ayodejioladimeji <layobright11@gmail.com>
2023-09-26 23:41:31 +0100

    completed navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,39 @@
 import Logo from '@/svgs/logo';
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="nav" style={{background:"black"}}>
+    <nav className='nav' style={{ background: 'black' }}>
       <div className='container'>
         <div className='row'>
-          <div className='col-6'>
+          <div className='col-5'>
             <div className='nav-left'>
-              <Logo/>
+              <Logo />
             </div>
           </div>
 
-          <div className='col-6'>
+          <div className='col-7'>
             <div className='nav-right'>
               <ul>
-                <li>The Firm</li>
-                <li>What we do</li>
-                <li>Insights</li>
-                <li>Client access</li>
-                <li>Schedule a call</li>
+                <li>
+                  <Link href='/'>The Firm</Link>
+                  <i className='bi bi-chevron-down'></i>
+                </li>
+                <li>
+                  <Link href='/'>What we do</Link>
+                  <i className='bi bi-chevron-down'></i>
+                </li>
+                <li>
+                  <Link href='/'>Insights</Link>
+                  <i className='bi bi-chevron-down'></i>
+                </li>
+                <li>
+                  <Link href='/'>Client access</Link>
+                </li>
+                <li className='schedule'>
+                  <Link href='/'>Schedule a call</Link>
+                </li>
               </ul>
             </div>
           </div>
