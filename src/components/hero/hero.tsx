cdd6d7a198d2b6ef38cb5de3275b62ba90602839
Ayodejioladimeji <layobright11@gmail.com>
2023-09-27 11:56:21 +0100

    implemented the hero section

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,18 +1,24 @@
+import Image from 'next/image';
 import React from 'react'
+
 
 const Hero = () => {
   return (
-    <div className='container-fluid bg-success'>
-      <div className='row'>
-        <div className='col-5 left'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col bg-primary'>col</div>
-            </div>
-          </div>
+    <div className='hero'>
+      <div className='hero-left'>
+        <div>
+          <h1>
+            Wealth Advisors to successful Individuals, Families & Businesses
+          </h1>
+          <p>
+            Our clients always come first. We serve them through a global
+            network powered by partnership, integrity, advancing economy.
+          </p>
         </div>
+      </div>
 
-        <div className='col-7 right'>right</div>
+      <div className='hero-right'>
+        <Image src='/images/hero-image.svg' alt='' width={100} height={100} />
       </div>
     </div>
   );
