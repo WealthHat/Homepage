af728e0add21224090c48fbef89d95fc37b19ad5
Ayodejioladimeji <layobright11@gmail.com>
2023-10-01 17:13:58 +0100

    completed the different offers component

--- src/components/different/different.tsx
+++ src/components/different/different.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Heading from '../heading/heading';
 import ServiceCard from '@/common/service-card/service-card';
+import { offer } from '@/constant/offer';
 
 const Different = () => {
   return (
@@ -11,7 +12,35 @@ const Different = () => {
         content='WealthHat is the world’s largest alternative asset manager, with $1 trillion. We serve institutional and individual investors by building strong businesses '
       />
       <div className='container different-center'>
-       
+        <div className='table-container'>
+          <table>
+            <thead>
+              <tr>
+                <th className='white' scope='col'></th>
+                <th scope='col'>WealthHat</th>
+                <th className='white' scope='col'>
+                  Traditional Firm
+                </th>
+                <th scope='col'>Traditional Firm</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {offer?.map((item: any, index: number) => {
+                return (
+                  <tr className='shaded' key={index}>
+                    <td className='white'>{item.name}</td>
+
+                    <td className="check">{item.wealthhat ? <i className="bi bi-check-circle-fill"></i> : ""}</td>
+
+                    <td className='white check'>{item.firstFirm ? <i className="bi bi-check-circle-fill"></i> : ""}</td>
+                    <td className="check">{item.secondFirm ? <i className="bi bi-check-circle-fill"></i> : ""}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
---
+++ src/constant/offer.tsx
@@ -0,0 +1,62 @@
+export const offer = [
+  {
+    name: 'Dedicated financial advisor',
+    wealthhat: true,
+    firstFirm: true,
+    secondFirm: true,
+  },
+  {
+    name: 'In-house tax advisors',
+    wealthhat: true,
+    firstFirm: false,
+    secondFirm: false,
+  },
+  {
+    name: 'Digital net worth tracking including stock options,crypto, and real estate',
+    wealthhat: true,
+    firstFirm: false,
+    secondFirm: false,
+  },
+  {
+    name: 'Cash flow and retirement projections',
+    wealthhat: true,
+    firstFirm: true,
+    secondFirm: false,
+  },
+  {
+    name: 'Public equities & fixed income',
+    wealthhat: true,
+    firstFirm: true,
+    secondFirm: true,
+  },
+  {
+    name: 'Vetted alternative investments',
+    wealthhat: true,
+    firstFirm: false,
+    secondFirm: false,
+  },
+  {
+    name: 'Tax return preparation and filing',
+    wealthhat: true,
+    firstFirm: false,
+    secondFirm: true,
+  },
+  {
+    name: 'Help with startup equity backed loans',
+    wealthhat: true,
+    firstFirm: false,
+    secondFirm: false,
+  },
+  {
+    name: 'Help with buying and selling secondaries',
+    wealthhat: true,
+    firstFirm: false,
+    secondFirm: false,
+  },
+  {
+    name: 'Help with getting a portfolio line of credit',
+    wealthhat: true,
+    firstFirm: false,
+    secondFirm: false,
+  }
+];
