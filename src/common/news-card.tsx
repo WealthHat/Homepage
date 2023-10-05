d28c32b7349e565f4538752982f8ace510ec8a5d
Ayodejioladimeji <layobright11@gmail.com>
2023-10-05 22:03:46 +0100

    completed all news page and components

---
+++ src/common/news-card.tsx
@@ -0,0 +1,22 @@
+import Arrow from '@/svgs/arrow';
+import React from 'react'
+
+const NewsCard = () => {
+  return (
+    <div className='news-card'>
+      <div className='news-box'>
+        <p>28 Aug 2023</p>
+        <h3>
+          WealthHat and Creative Planning Announce Agreement to Acquire
+          WealthHat Personal Financial Management Business
+        </h3>
+      </div>
+
+      <div className="arrow">
+        <i className="bi bi-chevron-right"></i>
+      </div>
+    </div>
+  );
+}
+
+export default NewsCard
---
+++ src/pages/news/index.tsx
@@ -0,0 +1,40 @@
+import NewsCard from '@/common/news-card';
+import Layout from '@/components/Layout';
+import Marker from '@/svgs/marker';
+import Image from 'next/image';
+import React from 'react';
+
+const News = () => {
+  return (
+    <Layout>
+      <div className='news'>
+        <div className='news-hero'>
+          <div className='container-fluid'>
+            <h1>News & Events</h1>
+            <p>See Our Latest News and Media Releases</p>
+          </div>
+        </div>
+
+        <div className="container news-body">
+          <div className="heading">
+            <h2>All News</h2>
+            <select className="form-select">
+              <option value="">All year</option>
+              <option value="">Monthly</option>
+              <option value="">Weekly</option>
+            </select>
+          </div>
+
+         <NewsCard/>
+         <NewsCard/>
+         <NewsCard/>
+         <NewsCard/>
+         <NewsCard/>
+
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default News;
