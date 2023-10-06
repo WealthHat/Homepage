79563a059672f05289ca55ad6854a4b9d4ba28df
Ayodejioladimeji <layobright11@gmail.com>
2023-10-06 08:56:24 +0100

    completed expertise component

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/Layout';
 import Marker from '@/svgs/marker';
+import Image from 'next/image';
 import React from 'react';
 
 const AboutUs = () => {
@@ -42,11 +43,9 @@ const AboutUs = () => {
               <div className='col-lg-6 left'>
                 <h3>A Firm with</h3>
 
-                <div className="d-flex align-items-center gap-3">
-                  <i className='mt-3 bi bi-dash-lg'></i> 
-                <h3>
-                  experience
-                </h3>
+                <div className='d-flex align-items-center gap-3'>
+                  <i className='mt-3 bi bi-dash-lg'></i>
+                  <h3>experience</h3>
                 </div>
                 <h3>and vision</h3>
               </div>
@@ -66,6 +65,38 @@ const AboutUs = () => {
                   development we have entrepreneurialism in our hearts and risk
                   awareness in our heads.
                 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='expertise'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-lg-6 left'>
+               <div>
+                 <div className="d-flex align-items-end">
+                  <h3>Our Expertise</h3>
+                   <i className='mt-3 bi bi-dash-lg'></i>
+                 </div>
+                <p>
+                  We see ourselves as a forward looking group of passionate
+                  property professionals with the relevant experience to match.
+                  With varied backgrounds in construction, investment and
+                  development we have entrepreneurialism in our hearts and risk
+                  awareness in our heads.
+                </p>
+                <p>
+                  We see ourselves as a forward looking group of passionate
+                  property professionals with the relevant experience to match.
+                  With varied backgrounds in construction, investment and
+                  development we have entrepreneurialism in our hearts and risk
+                  awareness in our heads.
+                </p>
+               </div>
+              </div>
+
+              <div className='col-lg-6 right'>
               </div>
             </div>
           </div>
