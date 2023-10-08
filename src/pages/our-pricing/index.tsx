2103dcd99824410b81bbd58a03a4744d391dee4e
Ayodejioladimeji <layobright11@gmail.com>
2023-10-08 17:35:46 +0100

    implemented our pricing hero section

---
+++ src/pages/our-pricing/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from '@/components/Layout';
+
+
+const OurPricing = () => {
+
+  // 
+  return (
+    <Layout>
+      <div className='our-pricing'>
+        <div className='pricing-hero'>
+          <div className='container-fluid'>
+            <h1>Our Pricing</h1>
+            <p>
+              Private Wealth Solutions’ mission is to deliver innovative
+              solutions with exceptional service to advisors and their clients.
+              We are committed to helping more investors unlock differentiated
+              opportunities beyond traditional asset classes.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default OurPricing;
