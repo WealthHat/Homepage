8a808b709a2b6b9ddd9ec5cf7754f63e21655d6a
Ayodejioladimeji <layobright11@gmail.com>
2023-11-19 21:24:25 +0100

    completed the firm pages

--- src/pages/our-process/index.tsx
+++ src/pages/our-process/index.tsx
@@ -3,6 +3,7 @@ import Layout from "@/components/Layout";
 import ClientCard from "@/common/client-card/client-card";
 import HeroSection from "@/common/hero-section/hero-section";
 import MetaTags from "@/components/meta-tags";
+import AnimatedWrapper from "@/common/animate/animateWrapper";
 
 const OurProcess = () => {
   return (
@@ -20,131 +21,145 @@ const OurProcess = () => {
           <div className="container">
             <div className="row">
               <div className="col-12 col-md-6 approach-left">
-                <div>
-                  <h1>WealthHat</h1>
-                  <h2>Approach</h2>
-                </div>
+                <AnimatedWrapper>
+                  <div>
+                    <h1>WealthHat</h1>
+                    <h2>Approach</h2>
+                  </div>
+                </AnimatedWrapper>
               </div>
 
               <div className="col-12 col-md-6 approach-right">
-                <p>
-                  The WealthHat Approach is the framework for how we work with
-                  our clients.
-                </p>
-                <p>
-                  {` It was designed with you in mind, it prioritizes
+                <AnimatedWrapper>
+                  <p>
+                    The WealthHat Approach is the framework for how we work with
+                    our clients.
+                  </p>
+                </AnimatedWrapper>
+
+                <AnimatedWrapper delay="300">
+                  <p>
+                    {` It was designed with you in mind, it prioritizes
                   accountability from us, and it reinforces positive financial
                   habits in our client's lives.`}
-                </p>
+                  </p>
+                </AnimatedWrapper>
               </div>
             </div>
 
-            <div className="timeline">
-              <div className="timeline-box right">
-                <div className="content">
-                  <small>Step one</small>
-                  <h2>Get to Know</h2>
-                  <p>
-                    A quick meeting to find out if working together makes sense.
-                  </p>
-                  <p>
-                    {`The goal with this meeting is to get to know you, your
+            <AnimatedWrapper>
+              <div className="timeline">
+                <div className="timeline-box right">
+                  <div className="content">
+                    <small>Step one</small>
+                    <h2>Get to Know</h2>
+                    <p>
+                      A quick meeting to find out if working together makes
+                      sense.
+                    </p>
+                    <p>
+                      {`The goal with this meeting is to get to know you, your
                     situation, and what you're looking for in a financial
                     planning relationship. We then share how we work with our
                     clients so you can leave this meeting knowing if we're the
                     right fit.`}
-                  </p>
+                    </p>
 
-                  <div className="time">
-                    Time before next meeting: 3 days to 2+ weeks
+                    <div className="time">
+                      Time before next meeting: 3 days to 2+ weeks
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="timeline-box left">
-                <div className="content">
-                  <small>Step two</small>
-                  <h2>Get to Know</h2>
-                  <p>
-                    A quick meeting to find out if working together makes sense.
-                  </p>
-                  <p>
-                    {` The goal with this meeting is to get to know you, your
+                <div className="timeline-box left">
+                  <div className="content">
+                    <small>Step two</small>
+                    <h2>Get to Know</h2>
+                    <p>
+                      A quick meeting to find out if working together makes
+                      sense.
+                    </p>
+                    <p>
+                      {` The goal with this meeting is to get to know you, your
                     situation, and what you're looking for in a financial
                     planning relationship. We then share how we work with our
                     clients so you can leave this meeting knowing if we're the
                     right fit.`}
-                  </p>
+                    </p>
 
-                  <div className="time">
-                    Time before next meeting: 3 days to 2+ weeks
+                    <div className="time">
+                      Time before next meeting: 3 days to 2+ weeks
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="timeline-box right">
-                <div className="content">
-                  <small>Step three</small>
-                  <h2>Get to Know</h2>
-                  <p>
-                    A quick meeting to find out if working together makes sense.
-                  </p>
-                  <p>
-                    {`The goal with this meeting is to get to know you, your
+                <div className="timeline-box right">
+                  <div className="content">
+                    <small>Step three</small>
+                    <h2>Get to Know</h2>
+                    <p>
+                      A quick meeting to find out if working together makes
+                      sense.
+                    </p>
+                    <p>
+                      {`The goal with this meeting is to get to know you, your
                     situation, and what you're looking for in a financial
                     planning relationship. We then share how we work with our
                     clients so you can leave this meeting knowing if we're the
                     right fit.`}
-                  </p>
+                    </p>
 
-                  <div className="time">
-                    Time before next meeting: 3 days to 2+ weeks
+                    <div className="time">
+                      Time before next meeting: 3 days to 2+ weeks
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="timeline-box left">
-                <div className="content">
-                  <small>Step four</small>
-                  <h2>Get to Know</h2>
-                  <p>
-                    A quick meeting to find out if working together makes sense.
-                  </p>
-                  <p>
-                    {`The goal with this meeting is to get to know you, your
+                <div className="timeline-box left">
+                  <div className="content">
+                    <small>Step four</small>
+                    <h2>Get to Know</h2>
+                    <p>
+                      A quick meeting to find out if working together makes
+                      sense.
+                    </p>
+                    <p>
+                      {`The goal with this meeting is to get to know you, your
                     situation, and what you're looking for in a financial
                     planning relationship. We then share how we work with our
                     clients so you can leave this meeting knowing if we're the
                     right fit.`}
-                  </p>
+                    </p>
 
-                  <div className="time">
-                    Time before next meeting: 3 days to 2+ weeks
+                    <div className="time">
+                      Time before next meeting: 3 days to 2+ weeks
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="timeline-box right">
-                <div className="content">
-                  <small>Step five</small>
-                  <h2>Get to Know</h2>
-                  <p>
-                    A quick meeting to find out if working together makes sense.
-                  </p>
-                  <p>
-                    {` The goal with this meeting is to get to know you, your
+                <div className="timeline-box right">
+                  <div className="content">
+                    <small>Step five</small>
+                    <h2>Get to Know</h2>
+                    <p>
+                      A quick meeting to find out if working together makes
+                      sense.
+                    </p>
+                    <p>
+                      {` The goal with this meeting is to get to know you, your
                     situation, and what you're looking for in a financial
                     planning relationship. We then share how we work with our
                     clients so you can leave this meeting knowing if we're the
                     right fit.`}
-                  </p>
+                    </p>
 
-                  <div className="time">
-                    Time before next meeting: 3 days to 2+ weeks
+                    <div className="time">
+                      Time before next meeting: 3 days to 2+ weeks
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </AnimatedWrapper>
           </div>
         </div>
       </div>
